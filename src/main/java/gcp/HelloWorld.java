c27641698e726ff022f3fe2c4bd72b5e11f6aa33
Shakti Sharma <shakti.sharma@conns.com>
2024-07-18 22:56:09 -0500

    initail commit

--- src/main/java/gcp/HelloWorld.java
+++ src/main/java/gcp/HelloWorld.java
@@ -13,6 +13,6 @@ public class HelloWorld implements HttpFunction {
   public void service(HttpRequest request, HttpResponse response)
       throws IOException {
     BufferedWriter writer = response.getWriter();
-    writer.write("Hello World!");
+    writer.write("Hello World with change!");
   }
 }
